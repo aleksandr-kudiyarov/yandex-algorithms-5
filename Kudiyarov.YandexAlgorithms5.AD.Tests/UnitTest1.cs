7aecf07c149d94614b80813495fc369df3e2bbb3
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2024-03-05 00:47:54 +0400

    lesson a task d in progress

---
+++ Kudiyarov.YandexAlgorithms5.AD.Tests/UnitTest1.cs
@@ -0,0 +1,72 @@
+using Kudiyarov.YandexAlgorithms5.BaseTest;
+
+namespace Kudiyarov.YandexAlgorithms5.AD.Tests;
+
+public class UnitTest1 : BaseUnitTest
+{
+    private const string Input01 = """
+                                   ********
+                                   ********
+                                   *R******
+                                   ********
+                                   ********
+                                   ********
+                                   ********
+                                   ********
+                                   
+                                   """;
+
+    private const string Output01 = """
+                                    49
+                                    
+                                    """;
+
+    private const string Input02 = """
+                                   ********
+                                   ********
+                                   ******B*
+                                   ********
+                                   ********
+                                   ********
+                                   ********
+                                   ********
+                                   
+                                   """;
+
+    private const string Output02 = """
+                                    54
+                                    
+                                    """;
+
+    private const string Input03 = """
+                                   ********
+                                   *R******
+                                   ********
+                                   *****B**
+                                   ********
+                                   ********
+                                   ********
+                                   ********
+                                   
+                                   """;
+
+    private const string Output03 = """
+                                    40
+                                    
+                                    """;
+
+    [Theory]
+    [InlineData(Input01, Output01)]
+    [InlineData(Input02, Output02)]
+    [InlineData(Input03, Output03)]
+    public void Test1(string input, string output)
+    {
+        InnerTest(input, output);
+    }
+
+    protected override string GetActual(string input)
+    {
+        var arr = input.Split(Environment.NewLine);
+        return Program.Solution(arr);
+    }
+}
---
+++ Kudiyarov.YandexAlgorithms5.AD/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace Kudiyarov.YandexAlgorithms5.AD;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var input = File.ReadAllLines("input.txt");
+        var solution = Solution(input);
+        File.WriteAllText("output.txt", solution);
+    }
+
+    public static string Solution(string[] input)
+    {
+        return InnerSolution(input).ToString();
+    }
+
+    private static int InnerSolution(string[] input)
+    {
+        var board = GetBoard(input);
+        return 0;
+    }
+
+    private static Board GetBoard(string[] input)
+    {
+        const int size = 8;
+
+        var pieces = new List<Piece>();
+
+        var board = new Board
+        {
+            Pieces = pieces
+        };
+        
+        for (var i = 0; i < size; i++)
+        {
+            for (var j = 0; j < size; j++)
+            {
+                var pieceType = GetPiece(input[i][j]);
+
+                if (pieceType is PieceType.Bishop or PieceType.Rook)
+                {
+                    var piece = new Piece
+                    {
+                        Type = pieceType,
+                        X = i,
+                        Y = j
+                    };
+                    
+                    pieces.Add(piece);
+                }
+                
+                board.Grid[i, j] = pieceType;
+            }
+        }
+
+        return board;
+    }
+
+    private static PieceType GetPiece(char ch)
+    {
+        switch (ch)
+        {
+            case '*':
+                return PieceType.Empty;
+            case 'r':
+            case 'R':
+                return PieceType.Rook;
+            case 'b':
+            case 'B':
+                return PieceType.Bishop;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(ch), ch, "Char is invalid");
+        }
+    }
+}
+
+public class Board
+{
+    public PieceType[,] Grid { get; } = new PieceType[8,8];
+    public IReadOnlyList<Piece> Pieces { get; set; }
+}
+
+public class Piece
+{
+    public PieceType Type { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+}
+
+public enum PieceType : byte
+{
+    Empty = 0,
+    Rook = 1,
+    Bishop = 2
+}
